89e9d07baec4d13af0d02264c24c250705b54bef
Karel Malbroukou <karel.malbroukou@blockchaintech.net.au>
2018-10-19 09:32:49 +1100

    add delayed URLs to be able to test slow requests and scaling

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,4 +9,10 @@ routes.get('/', (req, res) => {
   res.json({ status: "OK" })
 })
 
+routes.get('/delay', (req, res) => {
+  setTimeout( () => {
+    res.json({ status: "OK", delayed: 500 })
+  }, 500)
+})
+
 export default routes
